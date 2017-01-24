800ff0136fdc34f1ce591c00727a3b49d539deb4
Henry Medina <henry@aligncommerce.com>
2017-01-24 13:26:17 +0800

    Spring Java Framework

--- src/main/java/com/ekiras/ServletInitializer.java
+++ src/main/java/com/ekiras/ServletInitializer.java
@@ -1,11 +1,10 @@
 package com.ekiras;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
